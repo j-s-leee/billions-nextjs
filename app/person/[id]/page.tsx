e4519516f5b0b5f6f83fb2adbd504132b4472697
j-s-leee <q216044@gmail.com>
2025-01-24 00:04:14 +0900

    initial commit

---
+++ app/person/[id]/page.tsx
@@ -0,0 +1,25 @@
+import { notFound } from "next/navigation";
+
+async function getBillion(id: string) {
+  const response = await fetch(
+    `https://billions-api.nomadcoders.workers.dev/person/${id}`,
+    {
+      method: "GET",
+    }
+  );
+  const data = await response.json();
+  return data;
+}
+
+export default async function BillionDetail({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id } = await params;
+  if (!id) return notFound();
+
+  const billion = await getBillion(id);
+  console.log(billion);
+  return <div>{id}</div>;
+}
---
+++ components/billion-card.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export interface IListBillion {
+  map: any;
+  id: string;
+  name: string;
+  squareImage: string;
+  netWorth: number;
+  industries: Array<string>;
+}
+
+export default function BillionCard({
+  id,
+  name,
+  squareImage,
+  netWorth,
+  industries,
+}: IListBillion) {
+  return (
+    <Link
+      href={`/person/${id}`}
+      key={id}
+      className="card bg-base-100 w-96 shadow-xl"
+    >
+      <figure>
+        <img src={squareImage} alt={name} />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {name}
+          <div className="badge badge-secondary">{netWorth}</div>
+        </h2>
+        <div className="card-actions justify-end">
+          {industries.map((industry) => (
+            <div className="badge badge-outline">{industry}</div>
+          ))}
+        </div>
+      </div>
+    </Link>
+  );
+}
